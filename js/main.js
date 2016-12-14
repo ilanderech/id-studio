c2f40329dd147c691d0c5eb1d64c7324209954e0
ilanderech <ilanderech@gmail.com>
2016-12-13 20:26:58 -0600

    update 70%

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".header").removeClass("active");
+    }
+});
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".linkH").addClass("linkHA");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".linkH").removeClass("linkHA");
+    }
+});
+
+
+$(document).ready(function() {
+    var $lightbox = $('#lightbox');
+    
+    $('[data-target="#lightbox"]').on('click', function(event) {
+        var $img = $(this).find('img'), 
+            src = $img.attr('src'),
+            alt = $img.attr('alt'),
+            css = {
+                'maxWidth': $(window).width() - 100,
+                'maxHeight': $(window).height() - 100
+            };
+    
+        $lightbox.find('.close').addClass('hidden');
+        $lightbox.find('img').attr('src', src);
+        $lightbox.find('img').attr('alt', alt);
+        $lightbox.find('img').css(css);
+    });
+    
+    $lightbox.on('shown.bs.modal', function (e) {
+        var $img = $lightbox.find('img');
+            
+        $lightbox.find('.modal-dialog').css({'width': $img.width()});
+        $lightbox.find('.close').removeClass('hidden');
+    });
+});
